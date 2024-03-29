67af0922a903c23fcd7e9904c5b79e2ffdc3dc72
Adrian Maulana <adriangamer955@Gmail.com>
2024-03-29 10:38:52 +0700

    Initial Commit

---
+++ app/src/main/java/org/d3if0037/warungbapakpagi/ui/screen/AboutScreen.kt
@@ -0,0 +1,55 @@
+package org.d3if0037.warungbapakpagi.ui.screen
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.d3if0037.warungbapakpagi.R
+import org.d3if0037.warungbapakpagi.ui.theme.WarungBapakPagiTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AboutScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ) { padding ->
+        Text(
+            text = stringResource(id = R.string.Test),
+            modifier = Modifier
+                .padding(padding)
+                .padding(16.dp)
+        )
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun AboutPreview() {
+    WarungBapakPagiTheme {
+        AboutScreen()
+    }
+}
