f4bf9fbc3fa4845d71759583c2f8f8aa7b6631fc
Adrian Maulana <adriangamer955@Gmail.com>
2024-05-07 00:25:37 +0700

    membuat model

---
+++ app/src/main/java/org/d3if0037/warungbapakpagi/model/DetailTransaksi.kt
@@ -0,0 +1,6 @@
+package org.d3if0037.warungbapakpagi.model
+
+data class DetailTransaksi(
+    val id_p: Long,
+    val catatan: String
+)
---
+++ app/src/main/java/org/d3if0037/warungbapakpagi/model/Menu.kt
@@ -0,0 +1,7 @@
+package org.d3if0037.warungbapakpagi.model
+
+data class Menu(
+    val id: Long,
+    val judul: String,
+    val harga: String,
+)
